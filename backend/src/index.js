ece122a0ebbc0c3378226ca5b0445940c43b6cb9
GUILHERME LUIZELLI <gdlnani.89@gmail.com>
2020-04-11 01:16:43 -0300

    outros

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
  */
 const express = require('express');
 const cors = require('cors');
+const { errors} = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -13,5 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());//para evitar o status code 500(ou seja, nossa aplicação não soube se comportar com esse erro)
 
 app.listen(3333);
